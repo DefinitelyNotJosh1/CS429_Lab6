9e57e8747eee539f7066461a23283c3c4f4144e8
DefinitelyNotJosh1 <157744625+DefinitelyNotJosh1@users.noreply.github.com>
2025-03-14 11:07:59 -0700

    working model

--- Lab5Starter.py
+++ Lab5Starter.py
@@ -6,31 +6,65 @@ NUM_CENTROIDS=8
 
 data = pd.read_csv('synthetic.txt', delimiter=' ', header=None, names=['X','Y'])
 
+# random seed for reproducibility, this one gets a great result
+np.random.seed(47)
+
 X = data['X']
 Y = data['Y']
-print(X)
-print(Y)
-#randomly generate centroid indexes
-C = np.random.choice(np.shape(X)[0], NUM_CENTROIDS)
-print(C)
+
+# init random centroids
+centroids_x = np.random.randint(np.min(X), np.max(X), NUM_CENTROIDS)
+centroids_y = np.random.randint(np.min(Y), np.max(Y), NUM_CENTROIDS)
 
 Distance = np.zeros((np.shape(X)[0], NUM_CENTROIDS))
 
-epochs = 1
+epochs = 1000
 
-for i in epochs:
+for i in range(epochs):
+    print(f"Epoch {i+1}/{epochs}")
+    
+    # calculate distance from each point to each centroid
     for j in range(NUM_CENTROIDS):
-        Distance[:,j] = np.sqrt((X-C[j])**2 + (Y-C[j])**2)
-    print(Distance)
-
-
-
+        Distance[:,j] = np.sqrt((X - centroids_x[j])**2 + (Y - centroids_y[j])**2)
+    
+    # find the minimum distance for each point
+    min_distance = np.min(Distance, axis=1)
+    
+    # find the index of the minimum distance for each point
+    min_distance_index = np.argmin(Distance, axis=1)
+    
+    # update the centroids to the data point closest to the mean of each cluster
+    for j in range(NUM_CENTROIDS):
+        points_in_cluster = min_distance_index == j
+        if np.sum(points_in_cluster) > 0:
+            # calculate the mean of this cluster
+            mean_x = np.mean(X[points_in_cluster])
+            mean_y = np.mean(Y[points_in_cluster])
+            
+            # find the point closest to the mean
+            cluster_points_x = X[points_in_cluster]
+            cluster_points_y = Y[points_in_cluster]
+            
+            # calculate distances from each point in cluster to the mean
+            distances_to_mean = np.sqrt((cluster_points_x - mean_x)**2 + (cluster_points_y - mean_y)**2)
+            
+            # find the index of the closest point within this cluster
+            closest_point_idx = np.argmin(distances_to_mean)
+            
+            # set the centroid to be this closest point
+            centroids_x[j] = cluster_points_x.iloc[closest_point_idx]
+            centroids_y[j] = cluster_points_y.iloc[closest_point_idx]
+        else:
+            # if no points in cluster, pick a random point
+            random_idx = np.random.randint(0, len(X))
+            centroids_x[j] = X.iloc[random_idx]
+            centroids_y[j] = Y.iloc[random_idx]
 
 plt.xlabel('x')
-plt.xlabel('y')
+plt.ylabel('y')
 plt.title('Clustering')
 
-#plot data (black) and centroids (yellow)
-plt.scatter(X,Y)
-plt.scatter(X[C],Y[C],c='y')
+#plot data (blue) and centroids (red)
+plt.scatter(X,Y,c='b')
+plt.scatter(centroids_x,centroids_y,c='r')
 plt.show()
