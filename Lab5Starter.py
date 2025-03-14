b036a0a83444e8ebaa1530d2c29bc889d034d04f
DefinitelyNotJosh1 <157744625+DefinitelyNotJosh1@users.noreply.github.com>
2025-03-14 09:51:25 -0700

    Initial commit

---
+++ Lab5Starter.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+NUM_CENTROIDS=8
+
+data = pd.read_csv('synthetic.txt', delimiter=' ', header=None, names=['X','Y'])
+
+X = data['X']
+Y = data['Y']
+print(X)
+print(Y)
+#randomly generate centroid indexes
+C = np.random.choice(np.shape(X)[0], NUM_CENTROIDS)
+print(C)
+
+Distance = np.array()
+
+epochs = 1
+
+for i in epochs:
+    
+
+
+
+
+plt.xlabel('x')
+plt.xlabel('y')
+plt.title('Clustering')
+
+#plot data (black) and centroids (yellow)
+plt.scatter(X,Y)
+plt.scatter(X[C],Y[C],c='y')
+plt.show()
